e3c5dc8bfd1da71106d26239c5695fa5f7d8bd9d
Adam Polkosnik <apolkosnik@gmail.com>
2013-01-09 01:07:55 -0500

    Update files/src/kludge-winxp.bat
    Removed locking

--- files/src/kludge-winxp.bat
+++ files/src/kludge-winxp.bat
@@ -1,27 +1,6 @@
 @echo off
 REM Please email nick@theinterw3bs.com any changes or modifications to Kludge 4.0
 
-REM mutex using a lock file
-
-if [not] exist "c:\windows\temp\kludge.lck" (
-	echo kludge-wmics.bat did not run! . Exiting ...
-    exit /b 1
-)
-
-if exist "c:\windows\temp\kludge-wmics.bat.lck" (
-	echo kludge-wmics.bat is still running?! Exiting ...
-    exit /b 1
-)
-
-if exist "c:\windows\temp\kludge-winxp.bat.lck" (
-	echo kludge-winxp.bat is already running. Exiting ...
-    exit /b 1
-)
-
-copy NUL "c:\windows\temp\kludge-winxp.bat.lck"
-
-
-
 REM Set the title to show what is running
 title kludge-winxp.bat[start]
 
@@ -651,9 +630,6 @@ REM echo.
 echo Writing out done.txt
 echo %date% - %time% > c:\windows\temp\analysis\done.txt
 
-REM clear our lock file
-del "c:\windows\temp\kludge-winxp.bat.lck"
-del "c:\windows\temp\kludge.lck"
 title kludge-winxp.bat[end]
 
 REM END OF SCRIPT *******************************************************************************************************************
